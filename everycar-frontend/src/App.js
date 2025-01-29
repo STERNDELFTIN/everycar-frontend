04a01711e578d21b838dae729b7c1d094aaa07f4
delftin <resendo13@gmail.com>
2025-01-29 22:53:48 +0900

    상단 메뉴 및 메인 페이지 URL('/')
    - 상단 메뉴 배치 및 스타일 적용
    - 각각의 메뉴 링크

--- everycar-frontend/src/App.js
+++ everycar-frontend/src/App.js
@@ -1,10 +1,42 @@
-import logo from './logo.svg';
 import './App.css';
+import { Routes, Route, Link } from 'react-router-dom';
+import styled from 'styled-components';
+
+import Main from './routes/Main';
 
 function App() {
+
+  const LinkStyle = styled(Link)`color: black; text-decoration: none; `;
+
   return (
     <div className="App">
       
+      {/* 페이지 URL */}
+      <Routes>
+        <Route path='/' element={<Main />}></Route>
+      </Routes>
+
+      {/* 상단 메뉴 링크 */}
+      <nav className='menu'>
+        <div className='menu-container'>
+          <div className='left-menu'>
+          <LinkStyle to='/'><img src='/logo.png' style={ {width:'138px', height:'50px'} } /></LinkStyle>
+          </div>
+          <div className='center-menu'>
+            <LinkStyle to='/speedReservation'>빠른예약</LinkStyle>
+            <LinkStyle to='/callService'>부름서비스</LinkStyle>
+            <LinkStyle to='/shortRent'>단기렌트</LinkStyle>
+            <LinkStyle to='/longRent'>장기렌트</LinkStyle>
+            <LinkStyle to='/event'>이벤트</LinkStyle>
+          </div>
+          <div className='right-menu'>
+            <LinkStyle to='/login' className='login'>로그인</LinkStyle>
+            <LinkStyle to='/register' className='register'>회원가입</LinkStyle>
+          </div>
+        </div>
+      </nav>
+
+
     </div>
   );
 }
