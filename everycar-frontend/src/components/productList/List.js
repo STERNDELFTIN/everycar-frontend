af3b029e16ab68bbbd54f719838e6d71ec52c4bd
yesuel <adt04@naver.com>
2025-02-06 12:50:32 +0900

    카드 리스트 플렉스 간격 수정

--- everycar-frontend/src/components/productList/List.js
+++ everycar-frontend/src/components/productList/List.js
@@ -290,6 +290,66 @@ function List() {
               </div>
             </div>
 
+            <div class="card">
+              <div class="card-title">
+                <span class="card-name">EV6</span>
+                <span class="card-release">2021년식</span>
+              </div>
+              <img src={sample} alt="차 기종명" class="car-image"/>
+              <div class="card-icons">
+                <div class="card-icons-sm">
+                  <span class="material-symbols-outlined">star</span>
+                  <span class="icon-name">중형</span>
+                </div>
+                <div class="card-icons-sm">
+                  <span class="material-symbols-outlined">group</span>
+                  <span class="icon-name">5인승</span>
+                </div>
+                <div class="card-icons-sm">
+                  <span class="material-symbols-outlined">ev_station</span>
+                  <span class="icon-name">전기</span>
+                </div>
+                <div class="card-icons-sm">
+                  <span class="material-symbols-outlined">settings</span>
+                  <span class="icon-name">오토</span>
+                </div>
+              </div>
+              <div class="card-price">
+                <span class="card-price-sm1">월</span>
+                <span class="card-price-sm2">500,000원</span>
+              </div>
+            </div>
+
+            <div class="card">
+              <div class="card-title">
+                <span class="card-name">EV6</span>
+                <span class="card-release">2021년식</span>
+              </div>
+              <img src={sample} alt="차 기종명" class="car-image"/>
+              <div class="card-icons">
+                <div class="card-icons-sm">
+                  <span class="material-symbols-outlined">star</span>
+                  <span class="icon-name">중형</span>
+                </div>
+                <div class="card-icons-sm">
+                  <span class="material-symbols-outlined">group</span>
+                  <span class="icon-name">5인승</span>
+                </div>
+                <div class="card-icons-sm">
+                  <span class="material-symbols-outlined">ev_station</span>
+                  <span class="icon-name">전기</span>
+                </div>
+                <div class="card-icons-sm">
+                  <span class="material-symbols-outlined">settings</span>
+                  <span class="icon-name">오토</span>
+                </div>
+              </div>
+              <div class="card-price">
+                <span class="card-price-sm1">월</span>
+                <span class="card-price-sm2">500,000원</span>
+              </div>
+            </div>
+
             {/* 카드 반복 끝. +8개 반복. 총 9개 */}
 
           </div>
