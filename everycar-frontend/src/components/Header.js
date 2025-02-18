5c44341f5b30e094ad94660a6a423ba6009d663e
delftin <resendo13@gmail.com>
2025-02-18 09:54:32 +0900

    아이폰 상단메뉴 스타일 조정
    아이폰에서 상단메뉴의 position:absolute를 이용한 위치 스타일 적용 안되는 문제 해결

--- everycar-frontend/src/components/Header.js
+++ everycar-frontend/src/components/Header.js
@@ -5,7 +5,7 @@ import '../css/Header.css';
 
 function Header() {
 
-  {/* Link 태그 스타일 컴포넌트 */}
+  {/* Link 태그 스타일 컴포넌트 */ }
   const MenuLinkStyle = styled(Link)`color: black; text-decoration: none; `;
 
   return (
@@ -14,16 +14,19 @@ function Header() {
       {/* 상단 메뉴 링크 */}
       <nav className='menu'>
         <div className='menu-container'>
-          <div className='left-menu'>
-          <MenuLinkStyle to='/'><img src='/logo.png' style={ {width:'138px', height:'50px'} } /></MenuLinkStyle>
+          <div className='left-center-group'>
+            <div className='left-menu'>
+              <MenuLinkStyle to='/'><img src='/logo.png' style={{ width: '138px', height: '50px' }} /></MenuLinkStyle>
+            </div>
+            <div className='center-menu'>
+              <MenuLinkStyle to='/speedReservation'>빠른예약</MenuLinkStyle>
+              <MenuLinkStyle to='/callService'>부름서비스</MenuLinkStyle>
+              <MenuLinkStyle to='/shortRent'>단기렌트</MenuLinkStyle>
+              <MenuLinkStyle to='/longRent'>장기렌트</MenuLinkStyle>
+              <MenuLinkStyle to='/event'>이벤트</MenuLinkStyle>
+            </div>
           </div>
-          <div className='center-menu'>
-            <MenuLinkStyle to='/speedReservation'>빠른예약</MenuLinkStyle>
-            <MenuLinkStyle to='/callService'>부름서비스</MenuLinkStyle>
-            <MenuLinkStyle to='/shortRent'>단기렌트</MenuLinkStyle>
-            <MenuLinkStyle to='/longRent'>장기렌트</MenuLinkStyle>
-            <MenuLinkStyle to='/event'>이벤트</MenuLinkStyle>
-          </div>
+          
           <div className='right-menu'>
             <MenuLinkStyle to='/login' className='login'>로그인</MenuLinkStyle>
             <MenuLinkStyle to='/register' className='register'>회원가입</MenuLinkStyle>
