dfc71feeeb4fdb6c3d852c0d2e0401e85d6724b8
delftin <resendo13@gmail.com>
2025-02-20 15:12:48 +0900

    CarDetail.js -> 일반 스타일 적용 및 반응형 스타일 적용

--- everycar-frontend/src/routes/CarDetail.js
+++ everycar-frontend/src/routes/CarDetail.js
@@ -5,9 +5,15 @@ import styles from '../css/CarDetail.module.scss';
 
 import useCar from "../components/hooks/useCar";
 
+// 반응형 폰트 크기 계산 함수
+const vwFont = (min, max, baseWidth = 1920) => {
+    return `clamp(${min}px, calc(${max} / ${baseWidth} * 100vw), ${max}px)`;
+};
+
+// 부제목 스타일
 let SubTitleH3 = styled.h3`
-    margin-bottom: 10px;
-    font-size: 24px;
+    margin-bottom: ${vwFont(10, 19)};
+    font-size: ${vwFont(11, 24)};
 `;
 
 function CarDetail() {
@@ -27,17 +33,17 @@ function CarDetail() {
                 <h2>상세정보</h2>
             </div>
 
-            <div>
+            <div className={styles.carDetailContainer}>
                 <div className={styles.left}>
                     <RentTime title='대여시간' />
-                    <CarInfo title='제원정보' />
+                    <CarInfo title='제원정보' car={car} />
                     <CarOption title='차량옵션' />
                     <RentCondition title='대여조건' />
                     <ContractInfo title='계약정보' />
                 </div>
 
                 <div className={styles.right}>
-                    <ReservationInfo title={car.name} />
+                    <ReservationInfo title={car.name} car={car} />
                 </div>
             </div>
         </div>
@@ -47,31 +53,49 @@ function CarDetail() {
 // 대여시간
 function RentTime({ title }) {
     return (
-        <div>
+        <div className={`${styles.rentTimeContainer} ${styles.container}`}>
             <SubTitleH3>{title}</SubTitleH3>
-            <div></div>
+            <div className={styles.rentTime}>
+                <div className={styles.startRent}>
+                    <span className={styles.startDate}>2025. 01. 01</span><span className={styles.startTime}>10:00</span>
+                </div>
+                <img src="/images/icons/car-shape-icon.png" alt="Not Found" style={{ width: `${vwFont(10, 30)}`, height: `${vwFont(10, 30)}` }} />
+                <div className={styles.endRent}>
+                    <span className={styles.endDate}>2025. 01. 03</span><span className={styles.endTime}>12:00</span>
+                </div>
+            </div>
         </div>
     );
 }
 
 // 제원정보
-function CarInfo({ title }) {
+function CarInfo({ title, car }) {
+    let carInfo = [
+        { title: '제조사', content: '내용' },
+        { title: '등급', content: car.grade },
+        { title: '변속', content: '내용' },
+        { title: '연료', content: car.fuel },
+        { title: '인원', content: car.capacity },
+        { title: '연식', content: '내용' },
+    ];
+
     return (
-        <div>
+        <div className={`${styles.carInfoContainer} ${styles.container}`}>
             <SubTitleH3>{title}</SubTitleH3>
-            {/* <div>
+            <div className={styles.carName}>
                 <h5>{car.name}</h5>
                 <div>Premium</div>
             </div>
 
-            <div>
-                <p>제조사</p> <p></p>
-                <p>등급</p> <p>{car.grade}</p>
-                <p>변속</p> <p></p>
-                <p>연료</p> <p>{car.fuel}</p>
-                <p>탑승인원</p> <p>{car.capacity}</p>
-                <p>연식</p> <p></p>
-            </div> */}
+            <div className={styles.carInfo}>
+                {
+                    carInfo.map((item, i) =>
+                        <div key={i} className={styles.carInfoItem}>
+                            <p className={styles.carInfoTitle} style={{ width: '40%' }}>{item.title}</p><p className={styles.carInfoContent} style={{ width: '50%' }}>{item.content}</p>
+                        </div>
+                    )
+                }
+            </div>
         </div>
     )
 }
@@ -81,14 +105,14 @@ function CarOption({ title }) {
     let options = ['네비게이션', '하이패스', '블랙박스', '후방카메라', '열시트'];
 
     return (
-        <div>
+        <div className={`${styles.carOptionContainer} ${styles.container}`}>
             <SubTitleH3>{title}</SubTitleH3>
-            <div>
+            <div className={styles.carOptionBox}>
                 {
                     options.map((item, i) =>
-                        <div key={i}>
-                            <img src="/images/icons/car-option-icon.png" alt="img" />
-                            <p>{ item }</p>
+                        <div key={i} className={styles.carOption}>
+                            <img src="/images/icons/car-option-icon.png" alt="Not Found" style={{ width: vwFont(25, 60), height: vwFont(25, 60) }} />
+                            <p>{item}</p>
                         </div>
                     )
                 }
@@ -101,12 +125,12 @@ function CarOption({ title }) {
 function RentCondition({ title }) {
     let conditions = ['만26세 이상 성인', '면허 취득일로부터 1년', '2종 보통면허 이상 필요', '실물 면허증 소지자'];
     return (
-        <div>
+        <div className={`${styles.rentConditionContainer} ${styles.container}`}>
             <SubTitleH3>{title}</SubTitleH3>
             <div>
                 {
                     conditions.map((item, i) =>
-                        <div key={i}>
+                        <div key={i} className={styles.rentCondition}>
                             <svg width="16" height="16" viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg">
                                 <path d="M13.3337 4L6.00033 11.3333L2.66699 8" stroke="#5A5A5A" stroke-width="1.6" stroke-linecap="round" stroke-linejoin="round" />
                             </svg>
@@ -122,30 +146,40 @@ function RentCondition({ title }) {
 // 계약정보
 function ContractInfo({ title }) {
     return (
-        <div>
+        <div className={`${styles.contractInfoContainer} ${styles.container}`}>
             <SubTitleH3>{title}</SubTitleH3>
-            <div>
-                <h6>보험</h6>
-                <p>면허 취득일로부터 1년이 지난 만26세 이상의 성인부터 대여할 수 있습니다.</p>
+            <div className={styles.contractInfoBox}>
+                <p className={styles.contractTitle}>보험</p>
+                <p className={styles.contractContent}>면허 취득일로부터 1년이 지난 만26세 이상의 성인부터 대여할 수 있습니다.</p>
             </div>
         </div>
     );
 }
 
 // 우측 예약정보
-function ReservationInfo({ title }) {
+function ReservationInfo({ title, car }) {
     return (
-        <div>
-            {/* <img src={car.img || "/images/car-model/product-image-01.png"} alt={car.name} /> */}
-            <SubTitleH3>{title}</SubTitleH3>
-            <div style={{ border: '1px solid grey' }}></div>
-            <div>
-                <p>결제정보</p>
-                <p>총 대여료</p> <p>500,000원</p>
+        <div className={`${styles.reservationInfoContainer} ${styles.container}`}>
+            <div className={styles.carImage}>
+                <img src={car.img || "/images/car-model/product-image-01.png"} alt={car.name} style={{ height: vwFont(100, 200), width: 'auto' }} />
             </div>
-            <div>
-                <button>상담신청</button>
-                <button>예약하기</button>
+
+            <div className={styles.carContent}>
+                <SubTitleH3 className={styles.subTitle}>{title}</SubTitleH3>
+                <div className={styles.line} style={{ border: '1px solid #D9D9D9', marginBottom: vwFont(10, 15) }}></div>
+                <div className={styles.carInfoBox}>
+                    <div className={styles.priceInfoBox} style={{ marginBottom: vwFont(10, 15) }}>
+                        <p style={{ fontSize: vwFont(10, 18), fontWeight: '600', marginBottom: vwFont(10, 15)  }}>결제정보</p>
+                        <div className={styles.priceInfo}>
+                            <p style={{ marginLeft: '10px' }}>총대여료</p>
+                            <p style={{ fontSize: vwFont(12, 24), fontWeight: '600' }}>500,000원</p>
+                        </div>
+                    </div>
+                    <div className={styles.buttonsContainer}>
+                        <button className={styles.counselButton}>상담신청</button>
+                        <button className={styles.reservationButton}>예약하기</button>
+                    </div>
+                </div>
             </div>
         </div>
     );
