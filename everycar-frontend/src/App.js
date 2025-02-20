b7da79ccadb44f466920360bbdaab5c7a642e130
delftin <resendo13@gmail.com>
2025-02-20 11:18:06 +0900

    ★공통 페이지 부분 너비 및 높이 설정☆

--- everycar-frontend/src/App.js
+++ everycar-frontend/src/App.js
@@ -1,5 +1,6 @@
 import './App.css';
 import { Routes, Route } from 'react-router-dom';
+import styled from 'styled-components';
 
 import Header from './components/Header';
 import Footer from './components/Footer';
@@ -16,6 +17,15 @@ import Inquiry from './routes/Inquiry';
 import Login from './routes/Login';
 import Register from './routes/Register';
 
+let PageStyle = styled.div`
+  width: 72%;
+  height: auto;
+
+  @media (max-width: 480px) {
+    width: 87%;
+  }
+`;
+
 function App() {
 
   return (
@@ -25,30 +35,32 @@ function App() {
       <Header />
 
       {/* 페이지 URL */}
-      <Routes>
-        <Route path='/' element={<Main />}></Route>
-        
-        {/* 예약 관련 페이지 */}
-        <Route path='/reservation'>
-          <Route path='speedReservation' element={<SpeedReservation />}></Route>
-          <Route path='shortRent' element={<ShortRent />}></Route>
-          <Route path='carDetail/:id' element={<CarDetail />}></Route>
-        </Route>
-
-        {/* 고객 지원 관련 페이지 */}
-        <Route path='/support'>
-          <Route path='announcement' element={<Announcement />}></Route>
-          <Route path='event' element={<Event />}></Route>
-          <Route path='inquiry' element={<Inquiry />}></Route>
-        </Route>
-
-        {/* 인증 관련 페이지 */}
-        <Route path='/auth'>
-            <Route path='login' element={<Login />}></Route>
-            <Route path='register' element={<Register />}></Route>
+      <PageStyle>
+        <Routes>
+          <Route path='/' element={<Main />}></Route>
+          
+          {/* 예약 관련 페이지 */}
+          <Route path='/reservation'>
+            <Route path='speedReservation' element={<SpeedReservation />}></Route>
+            <Route path='shortRent' element={<ShortRent />}></Route>
+            <Route path='carDetail/:id' element={<CarDetail />}></Route>
           </Route>
 
-      </Routes>
+          {/* 고객 지원 관련 페이지 */}
+          <Route path='/support'>
+            <Route path='announcement' element={<Announcement />}></Route>
+            <Route path='event' element={<Event />}></Route>
+            <Route path='inquiry' element={<Inquiry />}></Route>
+          </Route>
+
+          {/* 인증 관련 페이지 */}
+          <Route path='/auth'>
+              <Route path='login' element={<Login />}></Route>
+              <Route path='register' element={<Register />}></Route>
+            </Route>
+
+        </Routes>
+      </PageStyle>
       
       {/* footer */}
       <Footer />
