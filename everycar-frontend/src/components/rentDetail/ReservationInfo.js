f7c0c976d581ed1581bef11938514032694cb4a4
delftin <resendo13@gmail.com>
2025-02-26 11:00:38 +0900

    조건 충족 시, 자동차 세부 페이지 -> 예약 및 결제 페이지

--- everycar-frontend/src/components/rentDetail/ReservationInfo.js
+++ everycar-frontend/src/components/rentDetail/ReservationInfo.js
@@ -1,8 +1,47 @@
+import { useNavigate } from 'react-router-dom';
+import { useState, useEffect } from 'react';
 import styles from '../../css/rentDetail/ReservationInfo.module.scss';
 import { vwFont } from '../../utils';
+import useEligibilityCheck from '../hooks/useEligibilityCheck';
+
+// Redux
+import { useDispatch } from 'react-redux';
+import { loginUser, logoutUser } from '../../redux/userSlice';
+
+// 더미 유저 데이터 가져오기
+import dummyUsers from '../../dummyData/dummyUser';
 
 // 우측 예약정보
 function ReservationInfo({ title, car, SubTitleH3 }) {
+    const navigate = useNavigate();
+    const dispatch = useDispatch();
+
+    // 자격여부 및 에러메시지
+    const { isEligible, errorMessage } = useEligibilityCheck();
+
+    // 자격 충족 시 예약 페이지로 넘어가기
+    const reservationHandler = () => {
+        if (isEligible) {
+            navigate("/reservation/rentReservation");
+        }
+    }
+
+    // 유저 테스트
+    const [selectedUser, setSelectedUser] = useState(dummyUsers[0]);
+
+   useEffect (() => { // 선택된 랜덤 유저
+    const randomUser = dummyUsers[Math.floor(Math.random() * dummyUsers.length)];
+    setSelectedUser(randomUser);
+
+    if (randomUser.isLoggedIn) {
+        dispatch(loginUser({ birthDate: randomUser.birthDate, licenseIssuedDate: randomUser.licenseIssuedDate }));
+    } else {
+        dispatch(logoutUser());
+    }
+
+    console.log(randomUser);
+    }, [dispatch]);
+
     return (
         <div className={`${styles.reservationInfoContainer} ${styles.container}`}>
             <div className={styles.carImage}>
@@ -22,8 +61,20 @@ function ReservationInfo({ title, car, SubTitleH3 }) {
                     </div>
                     <div className={styles.buttonsContainer}>
                         <button className={styles.counselButton}>상담신청</button>
-                        <button className={styles.reservationButton}>예약하기</button>
+                        {
+                            isEligible ? (
+                                <button
+                                    className={styles.reservationButton}
+                                    onClick={reservationHandler}
+                                >
+                                    예약하기
+                                </button>
+                            ) : (
+                                <p style={{ color: 'red', }}>{errorMessage}</p>
+                            )
+                        }
                     </div>
+
                 </div>
             </div>
         </div>
