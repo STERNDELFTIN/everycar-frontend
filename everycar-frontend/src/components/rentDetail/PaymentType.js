abffe75a45e99e62927537f8041d64621672da1a
delftin <resendo13@gmail.com>
2025-02-26 09:46:52 +0900

    예약및결제 페이지 - 결제유형, 약관 틀만 잡음

--- everycar-frontend/src/components/rentDetail/PaymentType.js
+++ everycar-frontend/src/components/rentDetail/PaymentType.js
@@ -1,7 +1,24 @@
+import { useState } from 'react';
+import styled from 'styled-components';
 import styles from '../../css/rentDetail/PaymentType.module.scss';
+import { vwFont } from '../../utils';
 
-function PaymentType({title, SubTitleH3}) {
-    
+function PaymentType({ title, SubTitleH3 }) {
+    return (
+        <div>
+            <SubTitleH3>{title}</SubTitleH3>
+            <label style={{display: 'flex',}}>
+                <input
+                    type='radio' />
+                <p>PayPal</p>
+            </label>
+            <label style={{display: 'flex',}}>
+                <input
+                    type='radio' />
+                <p>무통장 입금</p>
+            </label>
+        </div>
+    );
 }
 
 export default PaymentType;
