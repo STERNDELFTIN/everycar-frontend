6afe52e0a023deb1ba6eacc9c6b38f37c7efe24f
delftin <resendo13@gmail.com>
2025-02-27 16:14:15 +0900

    .

---
+++ everycar-frontend/src/routes/myPage/MyInfoManagement.js
@@ -0,0 +1,16 @@
+import styles from '../../css/routes/myPage/MyInfoManagement.module.scss';
+
+import MyPage from '../../components/common/MyPage';
+
+function MyInfoManagement() {
+    return (
+        <div className={styles.myInfoManagement}>
+            <MyPage />
+            <div>
+                    
+            </div>
+        </div>
+    );
+}
+
+export default MyInfoManagement;
