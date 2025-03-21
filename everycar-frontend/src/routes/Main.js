02d44d74ca46469d83519bf2a04f5db8d05e86bf
delftin <resendo13@gmail.com>
2025-03-21 16:31:46 +0900

    뒤로가기 방지 관련 주석 추가

--- everycar-frontend/src/routes/Main.js
+++ everycar-frontend/src/routes/Main.js
@@ -20,21 +20,21 @@ function Main() {
         };
 
         // setInterval()을 사용하여 지속적으로 pushState() 실행
-        const interval = setInterval(blockBackNavigation, 100);
+        const interval = setInterval(blockBackNavigation, 100); // 0.1초마다 현재 페이지를 히스토리에 덮어써 이전 페이지로 이동 불가능
 
         // 뒤로가기 버튼을 눌러도 다시 현재 페이지 유지
-        window.addEventListener("popstate", blockBackNavigation);
+        window.addEventListener("popstate", blockBackNavigation); // popstate : 사용자가 뒤로가기 버튼을 눌렀을 때 발생하는 이벤트
 
         // 사용자가 페이지를 떠나려 할 때 경고 메시지 표시 (새로고침 및 창 닫기 방지)
         window.onbeforeunload = (e) => {
-            e.preventDefault();
+            e.preventDefault(); // 실수로 페이지를 떠나는 것을 방지
             return (e.returnValue = "이 페이지를 떠나시겠습니까?");
         };
 
         return () => {
-            clearInterval(interval); // setInterval 정리 (메모리 누수 방지)
+            clearInterval(interval); // setInterval 정리 (메모리 누수 방지) -> 페이지가 변경되거나 컴포넌트가 언마운트될 때 해당 함수를 호출하여 메모리 정리
             window.removeEventListener("popstate", blockBackNavigation); // popstate 이벤트 제거
-            window.onbeforeunload = null; // 새로고침 방지 해제
+            window.onbeforeunload = null; // 새로고침 방지 해제 -> 페이지 변경된 후에도 이벤트리스너가 남아있어 예상치 못한 동작 발생 가능성 존재하므로 제거해줌
         };
     }, []);
 
