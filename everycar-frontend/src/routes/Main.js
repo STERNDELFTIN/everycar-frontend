500f8a728e155bc86e21f80a131153dc857af46f
seominhee <minheee07@gmail.com>
2025-03-23 22:18:59 +0900

    메인페이지 지도컨텐츠 추가

--- everycar-frontend/src/routes/Main.js
+++ everycar-frontend/src/routes/Main.js
@@ -8,6 +8,7 @@ import Shortcut from '../components/main/Shortcut.js';
 import Plan from '../components/main/Plan.js';
 // import Car from '../components/main/Car.js';
 import Popup from '../components/popup/PosAndPeriodPopup.js';
+import Map from '../components/main/Map.js';
 
 const TitleStyle = styled.h3`font-size: clamp(15px, 2.3vw, 32px); text-align: left; margin-bottom: 2.5vh; `;
 
@@ -45,6 +46,7 @@ function Main() {
             <Shortcut />
             <Plan title="에브리카 렌트 플랜" TitleStyle={TitleStyle} />
             {/* <Car title="국내 인기 차량" TitleStyle={TitleStyle} /> */}
+            <Map></Map>
         </div>
     );
 }
